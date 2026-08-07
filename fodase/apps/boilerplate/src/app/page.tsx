b7584591acadd692d4f22dcd63f91c5c2a44208e
italy <italy.christian@hotmail.com>
2026-08-06 22:42:19 -0300

    feat: create folders

--- fodase/apps/boilerplate/src/app/page.tsx
+++ fodase/apps/boilerplate/src/app/page.tsx
@@ -1,14 +1,9 @@
 import styles from './page.module.css';
 
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
   return (
     <div className={styles.page}>
-      
+
     <div className="wrapper">
       <div className="container">
         <div id="welcome">
@@ -454,7 +449,7 @@ export default function Index() {
         </p>
       </div>
     </div>
-  
+
     </div>
   );
 };
